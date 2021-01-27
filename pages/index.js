455614ac4829fcc8ceee0d8c2a9c0717a9184ba9
Lucaslva580 <lucasaraujo580@gmail.com>
2021-01-26 22:50:39 -0300

    Adicionando logo e corrigindo com eslint

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
-import styled from 'styled-components'
-import db from '../db.json';
-import Widget from '../src/components/Widget'
-import Footer from '../src/components/Footer'
-import QuizBackground from '../src/components/QuizBackground'
-import GitHubCorner from '../src/components/GitHubCorner'
+import React from 'react';
+import styled from 'styled-components';
+import Head from 'next/head';
+import { useRouter } from 'next/router'
 
+import db from '../db.json';
+import Widget from '../src/components/Widget';
+import QuizLogo from '../src/components/QuizLogo';
+import Footer from '../src/components/Footer';
+import QuizBackground from '../src/components/QuizBackground';
+import GitHubCorner from '../src/components/GitHubCorner';
 // const BackgroundImage = styled.div`
 // background-image: url(${db.bg});
 // flex:1;
@@ -24,26 +28,52 @@ export const QuizContainer = styled.div`
   `;
 
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = React.useState('');
+
   return (
     <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <title>Quiz - Shingeki</title>
+      </Head>
       <QuizContainer>
+        <QuizLogo />
         <Widget>
-            <Widget.Header>
-<h1>SHINGEKI NO KYOJIN</h1>
-            </Widget.Header>
+          <Widget.Header>
+            <h1>SHINGEKI NO KYOJIN</h1>
+          </Widget.Header>
           <Widget.Content>
-<p>Entreguem seus corações</p>
+            <form onSubmit={function (infosDoEvento) {
+              infosDoEvento.preventDefault();
+              router.push(`/quiz?name=${name}`);
+              console.log('Fazendo uma submissão por meio do react');
+
+              //router manda para a próxima página
+            }}
+            >
+              <input 
+              onChange={ function (infosDoEvento){
+                console.log(infosDoEvento.target.value);
+                // name = infosDoEvento.target.value;
+                setName(infosDoEvento.target.value);
+              }}
+              placeholder="Digite seu nome" />
+              <button type="submit" disabled={name.length === 0}>
+                JOGAR
+                {name}
+              </button>
+            </form>
           </Widget.Content>
         </Widget>
         <Widget>
-        <Widget.Content>
-<h1>Quizes de outros animes</h1>
-<p>Entreguem seus corações</p>
-</Widget.Content>
+          <Widget.Content>
+            <h1>Quizes de outros animes</h1>
+            <p>Entreguem seus corações</p>
+          </Widget.Content>
         </Widget>
-        <Footer/>
+        <Footer />
       </QuizContainer>
-        <GitHubCorner projectUrl="https://github.com/LucasLva580" />
+      <GitHubCorner projectUrl="https://github.com/LucasLva580" />
     </QuizBackground>
   );
 }
